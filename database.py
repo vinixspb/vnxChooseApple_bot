a80c35a14a041b86a2597f424c531a51ea0a6a53
vinixspb <60535818+vinixspb@users.noreply.github.com>
2025-12-11 16:15:21 +0200

    Create database.py

---
+++ database.py
@@ -0,0 +1,13 @@
+# database.py
+
+# Структура: Категория -> Список моделей
+CATALOG = {
+    "iphones": {
+        "label": "📱 iPhone",
+        "models": ["iPhone 15 Pro Max", "iPhone 15 Pro", "iPhone 14", "iPhone 13"]
+    },
+    "macbooks": {
+        "label": "💻 MacBook",
+        "models": ["MacBook Air M2", "MacBook Pro 14 M3"] # Заготовка на будущее
+    }
+}
